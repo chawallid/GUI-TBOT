d49afa00b5f66eb55557d6c0327ef225e1ed1e37
chawallid <22177340+chawallid@users.noreply.github.com>
2021-06-29 12:13:02 +0700

    readserial

---
+++ testReadSerial.py
@@ -0,0 +1,16 @@
+import serial
+
+ser = serial.Serial(
+    port='COM5',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0)
+count = 0
+while True:
+    for line in ser.read():
+        print(str(count) + str(': ') + chr(line) )
+        count = count+1
+
+ser.close()
